ffb4480b2928c53bcc435940b477643c8b787c15
Kalil Teotonio <80278976+Galill@users.noreply.github.com>
2023-06-07 11:34:23 -0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,14 @@
 #include "FileReader.h"
+#include "SortingAlgorithms.h"
 
 using namespace std;
 
 int main() {
 	FileReader file;
+	SortingAlgorithms sort;
 
 	file.readFile();
+	sort.RunAlgorithms();
 
 	return 0;
 }
