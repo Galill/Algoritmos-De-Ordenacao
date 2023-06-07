702c0d91c85159ec498407316c2fbb9ae0e93235
Kalil Teotonio <80278976+Galill@users.noreply.github.com>
2023-06-07 11:33:21 -0300

    Update SortingAlgorithms.h

--- SortingAlgorithms.h
+++ SortingAlgorithms.h
@@ -4,13 +4,19 @@
 class SortingAlgorithms {
 
 	public:
+		static std::vector<int> Vector;
+
 		void SelectionSort(std::vector<int> Vector);
 
 		void BubbleSort(std::vector<int> Vector);
 
 		void InsertionSort(std::vector<int> Vector);
 
-		void MergeSort();
+		void MergeSort(std::vector<int>& Vector, std::vector<int>& Vector_aux, int Start, int End);
+
+		void Merge(std::vector<int>& Vector, std::vector<int>& Vector_aux, int Start, int middle, int End);
 
 		void QuickSort();
+
+		void RunAlgorithms();
 };
